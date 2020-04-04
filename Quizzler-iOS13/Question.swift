afa0dc498c609865a4480fa57cd484b1c96c559f
김정태(Jeongtae Kim)/App개발팀/11ST <origogi.kim@sk.com>
2020-04-04 14:45:36 +0900

    [Implement] Implement Quiz structure

---
+++ Quizzler-iOS13/Question.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  Quizzler-iOS13
+//
+//  Created by 1101373 on 2020/04/04.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text : String
+    let answser : String
+}
--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -9,50 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var progressBar: UIProgressView!
+    @IBOutlet var trueButton: UIButton!
+    @IBOutlet var falseButton: UIButton!
 
-    @IBOutlet weak var questionLabel: UILabel!
-    @IBOutlet weak var progressBar: UIProgressView!
-    @IBOutlet weak var trueButton: UIButton!
-    @IBOutlet weak var falseButton: UIButton!
+    var question = 0
 
-    var question  = 0
-
-    let quiz           = [
-        ["Four + Two is equals Six", "True"],
-        ["Four - Three is greater than One", "True"],
-        ["Four + Eight is equals less than Ten", "False"]
+    let quiz = [
+        Question(text: "A slug's blood is green.", answser: "True"),
+        Question(text: "Approximately one quarter of human bones are in the feet.", answser: "True"),
+        Question(text: "The total surface area of two human lungs is approximately 70 square metres.", answser: "True"),
+        Question(text: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", answser: "True"),
+        Question(text: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", answser: "False"),
+        Question(text: "It is illegal to pee in the Ocean in Portugal.", answser: "True"),
+        Question(text: "You can lead a cow down stairs but not up stairs.", answser: "False"),
+        Question(text: "Google was originally called 'Backrub'.", answser: "True"),
+        Question(text: "Buzz Aldrin's mother's maiden name was 'Moon'.", answser: "True"),
+        Question(text: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", answser: "False"),
+        Question(text: "No piece of square dry paper can be folded in half more than 7 times.", answser: "False"),
+        Question(text: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", answser: "True")
     ]
 
-    override func viewDidLoad(){
+    override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
     }
 
-
     @IBAction func pressButton(_ sender: UIButton) {
         let userAnswer = sender.currentTitle
-        let actualAnswer = quiz[question][1]
-        
-        if (userAnswer == actualAnswer) {
+        let actualAnswer = quiz[question].answser
+
+        if userAnswer == actualAnswer {
             print("right!")
         }
         else {
             print("Wrong!")
         }
-        
+
         if question < quiz.count - 1 {
             question += 1
         }
         else {
             question = 0
         }
-        
-        updateUI()
 
+        updateUI()
     }
 
     func updateUI() {
-        questionLabel.text = quiz[question][0]
+        questionLabel.text = quiz[question].text
     }
-
 }
